9ad00b2fb565f373937e5acc65b5a34e585058ac
EfanovEvgeny1 <eugenye@bk.ru>
2024-11-19 12:57:16 +0300

    first commit

---
+++ project/catalog/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse 
+
+def index(request):
+    return render(request,"firstapp/index.html")
+
+def about(request):
+    return render(request,"firstapp/about.html")
+
+def contact(request):
+    return render(request,"firstapp/contact.html")
---
+++ project/project/urls.py
@@ -0,0 +1,10 @@
+from catalog import views 
+from django.contrib import admin
+from django.urls import path
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
+    path('about/', views.about, name='about'), 
+    path('contact/', views.contact, name='contact'),
+]
