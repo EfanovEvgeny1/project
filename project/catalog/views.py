fdba9f9ff0a1d6f4c80a205e2c88728df3886319
Your Name <you@example.com>
2024-12-09 11:19:46 +0300

    пз 16

--- project/catalog/views.py
+++ project/catalog/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse 
 from.forms import UserForm 
+from .models import Person
 
 def about(request):
     return render(request,"firstapp/about.html")
@@ -9,11 +10,13 @@ def contact(request):
     return render(request,"firstapp/contact.html")
 
 def index(request):     
-    my_text = 'Изучаем формы Django'
-    context = {'my_text': my_text} 
-    return render(request, "firstapp/index.html", context) 
+    my_text = 'Изучаем модели Django'
+    people_kol = Person.object_person.count()
+    context = {'my_text': my_text, "people_kol": people_kol}
+    return render(request, "firstapp/index.html", context)
 
 
+"""
 def my_form(request):     
     if request.method == "POST":
         userform = UserForm(request.POST)
@@ -25,3 +28,17 @@ def my_form(request):
         return HttpResponse(output)
     userform = UserForm()
     return render(request, "firstapp/my_form.html", {"form": userform})
+
+"""
+
+def my_form(request):
+    if request.method == "POST": 
+        form = UserForm(request.POST) 
+        if form.is_valid(): 
+            form.save()
+
+    my_text = 'Сведения о клиентах'
+    people = Person.object_person.all()
+    form = UserForm()
+    context = {'my_text': my_text, "people": people, "form": form}
+    return render(request, "firstapp/my_form.html", context)
--- project/project/urls.py
+++ project/project/urls.py
@@ -1,7 +1,8 @@
 from catalog import views 
 from django.contrib import admin
 from django.urls import path
-from django import forms 
+from django import forms
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
